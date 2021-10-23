92b51173317d53ae9e994871877ac4cc7c4d9e9b
Derrick Strong <derrickmstrong@gmail.com>
2021-10-23 09:30:26 -0500

    Format changes

--- 3.2_Express_modular_app/app.js
+++ 3.2_Express_modular_app/app.js
@@ -1,12 +1,15 @@
 const express = require('express'),
-    greetMiddleware = require('./greet'),
-    PORT = 8080;
+  greetMiddleware = require('./greet'),
+  PORT = 8080;
 
-    express()
-        // use = all HTTP methods
-        .use('/api/v1/', greetMiddleware({
-            greeting: 'Greeting World!'
-        }))
-        .listen(PORT, () => {
-            console.log(`Server running on port: ${PORT}`)
-        })
+express()
+  // use = all HTTP methods
+  .use(
+    '/api/v1/',
+    greetMiddleware({
+      greeting: 'Greeting World!',
+    })
+  )
+  .listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+  });
