c8813ce00f90d6e0bb713908f49e7669b0478a05
Derrick Strong <derrickmstrong@gmail.com>
2021-10-24 08:36:13 -0500

    3.13 Added comments

--- 3.13_Custom_middleware/index.js
+++ 3.13_Custom_middleware/index.js
@@ -1,14 +1,16 @@
 const express = require('express'),
-    app = express();
+  app = express();
 
-    // Custom middleware
+// Custom middleware - each request will pass through it
 app.use((req, res, next) => {
-    req.user = 'Derrick Strong'
-    next()
-})
+  req.user = 'Derrick Strong';
+  next(); // it will pass the control to next matching route
+});
 
-app.get('/', (req, res) => {
-    const user = req.user
-    console.log(user)
-    res.send(user)
-}).listen(3000)
+app
+  .get('/', (req, res) => {
+    const user = req.user;
+    console.log(user);
+    res.send(user);
+  })
+  .listen(3000);
